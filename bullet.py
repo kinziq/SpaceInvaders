85051ef729af520c0c08098b9dea77b6ce75ef97
kinziq <144268675+kinziq@users.noreply.github.com>
2023-11-15 14:27:49 -0500

    Create bullet.py

---
+++ bullet.py
@@ -0,0 +1,19 @@
+import pygame
+import sys
+#custom object for copys of walls        
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, startX,startY,width,height):
+        super().__init__()
+        self.shot=False
+        plr_load= pygame.image.load('images/bullet.png')
+        self.image = pygame.transform.scale(plr_load, (width,height)).convert_alpha()
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect = self.image.get_rect(topleft=(startX,startY))
+        self.movey = 2
+    def move(self):
+        self.rect.y-=self.movey
+        self.shot=True
+        
+    def update(self):
+        if self.shot:
+            self.rect.y-=self.movey
