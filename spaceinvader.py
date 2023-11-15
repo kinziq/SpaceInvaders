9f63d171830a4e8d4055e158247105eaf078cc76
kinziq <144268675+kinziq@users.noreply.github.com>
2023-11-15 14:27:25 -0500

    Update spaceinvader.py

--- spaceinvader.py
+++ spaceinvader.py
@@ -4,24 +4,25 @@ import sys
 class SpaceInvader(pygame.sprite.Sprite):
     def __init__(self, startX,startY,width,height,path_images):
         super().__init__()
-        plr_load= pygame.image.load('images/imgoated.png')
+        plr_load= pygame.image.load('images/spaceinvader.png')
         self.image = pygame.transform.scale(plr_load, (width,height)).convert_alpha()
         self.mask = pygame.mask.from_surface(self.image)
         self.rect = self.image.get_rect(topleft=(startX,startY))
+        self.movex = 2
+
     def move(self):
-        position = self.image.get_rect()
-        position = position.move(2, 0)
-    
-    #def shoot():
-        #plrinput = pygame.key.get_pressed()
-        #bullet = plrinput[pygame.K_SPACE]
+        self.rect.x+=self.movex
         
-        
-    def check_hit(self,group):
+    def SI_check_hit_X(self,group):
         if pygame.sprite.spritecollide(self, group, False, collided=pygame.sprite.collide_mask):
             return True
         else:
             return
         
-    def collide(self):
-        self.rect.x -= self.movex
+        
+        
+    def SI_collide_X(self):
+        self.rect.x-=self.movex
+        
+        
+            
