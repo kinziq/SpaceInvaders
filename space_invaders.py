ab4e7549dcce07ae2f192a3e7497e3bced90dd1e
kinziq <144268675+kinziq@users.noreply.github.com>
2023-11-15 14:26:10 -0500

    Update space_invaders.py

--- space_invaders.py
+++ space_invaders.py
@@ -4,6 +4,8 @@
 import pygame, sys
 from player import Character
 from spaceinvader import SpaceInvader
+from playarea import area
+from bullet import Bullet
 
 pygame.init()
 
@@ -16,46 +18,73 @@ WINDOW_HEIGHT = 500
 def exit_button():
     sys.exit()
 
+Invaderlocx = 50
+Invaderlocy = 20
+
+Playerlocx= 230
+Playerlocy= 450
+
+
 window = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT), pygame.HWSURFACE)
-pygame_icon = pygame.image.load('images/SpaceInvadericon.png')
-pygame.display.set_icon(pygame_icon)
-playersprite=Character(230,450,30,30,'images/imgoated.png')
-Invader=SpaceInvader(230,150,30,30,'images/imgoated.png')
-InvaderSpeed=4
+playersprite=Character(Playerlocx,Playerlocy,30,30,'images/imgoated.png')
+bg=area(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT,255)
+Invader=SpaceInvader(Invaderlocx,Invaderlocy,50,30,'images/spaceinvader.png')
+bullets=Bullet(250,250,30,30,'images/bullet.png')
 #Button_1=button.no_background(5, 0, "Arial",20,(0,0,0),(255,0,0),"exit",exit_button)
 
+
+
+Playarea = pygame.sprite.Group()
 Player = pygame.sprite.GroupSingle()
-Player.add(playersprite)
-
-### The Space Invaders
 Enemy = pygame.sprite.Group()
+bullet=pygame.sprite.Group()
+Playarea.add(bg)
+Player.add(playersprite)
 Enemy.add(Invader)
-
+bullet.add(bullets)
 
 pygame.display.set_caption("Space Invaders")
 def display():
     window.fill((255,255,255)) #White background
-    Player.draw(window)
+    Playarea.draw(window)
     Enemy.draw(window)
-    
-    
+    Player.draw(window)
+    bullet.draw(window)
+    Invader.move()
     
     
 while True:
     display()
-    playersprite.move(3)
+    playersprite.move(3)    
+    
+    
+    
+    # if pygame.sprite.spritecollide(bg, Enemy, False, collided=pygame.sprite.collide_mask):
+    #     row+=1
+    bullet.update()
     for event in pygame.event.get():
-      # if user  QUIT then the screen will close
+        
+        key_input = pygame.key.get_pressed()
+        if key_input[pygame.K_SPACE]:
+            bullets=Bullet(playersprite.rect.x,playersprite.rect.y,30,30)
+            bullet.add(bullets)
+            bullets.move()
+            bullet.draw(window)
+            
+        # if user QUIT then the screen will close
         if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-
-        #if playersprite.check_hit():
-            #playersprite.collide()
-       
+                pygame.quit()
+                sys.exit()
+        
+        if Invader.SI_check_hit_X(Playarea):
+            Invader.movex=-Invader.movex
+            Invader.rect.y+=20
+            
+        if playersprite.plr_check_hit(Playarea):
+            playersprite.plr_collide()
+        
     pygame.display.update() #update the display
     fpsClock.tick(fps) #speed of redraw
+        
     
     
-    
-    #https://www.pygame.org/docs/tut/MoveIt.html
