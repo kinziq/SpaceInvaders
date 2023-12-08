a3cbc1680e7e1e93d82767baca9de596ca5b1212
kinziq <144268675+kinziq@users.noreply.github.com>
2023-12-08 11:39:25 -0500

    Update playarea.py

--- playarea.py
+++ playarea.py
@@ -13,3 +13,15 @@ class area(pygame.sprite.Sprite):
         #self.image = pygame.transform.scale(crt_load, (width,height)).convert_alpha()
         #self.mask = pygame.mask.from_surface(self.image)
         #self.rect = self.image.get_rect(topleft=(startX,startY))
+class Background(pygame.sprite.Sprite):
+    def __init__(self, startX,startY,width,height,path_images):
+        super().__init__()
+        play_load= pygame.image.load('images/background.png')
+        #crt_load = pygame.image.load('images/crt.png')
+        self.image = pygame.transform.scale(play_load, (width,height)).convert_alpha()
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect = self.image.get_rect(topleft=(startX,startY))
+        #self.image = pygame.transform.scale(crt_load, (width,height)).convert_alpha()
+        #self.mask = pygame.mask.from_surface(self.image)
+        #self.rect = self.image.get_rect(topleft=(startX,startY))
+
