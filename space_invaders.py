faeafd4618bb71c623aec4b3eea8e9688e735177
kinziq <144268675+kinziq@users.noreply.github.com>
2023-12-08 11:39:52 -0500

    Update space_invaders.py

--- space_invaders.py
+++ space_invaders.py
@@ -1,11 +1,15 @@
 #Name: Kinz
 #Date: 25/10/23
 #Purpose: Make a space invaders game
-import pygame, sys
+import random
+import pygame
+import sys
 from player import Character
 from spaceinvader import SpaceInvader
 from playarea import area
+from playarea import Background
 from bullet import Bullet
+from Invaderbullet import Invader_Bullet
 
 pygame.init()
 
@@ -15,110 +19,130 @@ fpsClock = pygame.time.Clock()
 WINDOW_WIDTH = 500
 WINDOW_HEIGHT = 500
 
-def exit_button():
-    sys.exit()
+PLAYER_X = 230
+PLAYER_Y = 450
 
-Invaderlocx = 50
-Invaderlocy = 20
+INVADER_X = 50
+INVADER_Y = 30
 
-Playerlocx= 230
-Playerlocy= 450
-
-
-window = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT), pygame.HWSURFACE)
-playersprite=Character(Playerlocx,Playerlocy,30,30,'images/imgoated.png')
-bg=area(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT,255)
-Invader=SpaceInvader(Invaderlocx,Invaderlocy,50,30,'images/spaceinvader.png')
-Invader2=SpaceInvader(Invaderlocx+45,Invaderlocy,50,30,'images/spaceinvader.png')
-Invader3=SpaceInvader(Invaderlocx+90,Invaderlocy,50,30,'images/spaceinvader.png')
-Invader4=SpaceInvader(Invaderlocx+135,Invaderlocy,50,30,'images/spaceinvader.png')
-Invader5=SpaceInvader(Invaderlocx+180,Invaderlocy,50,30,'images/spaceinvader.png')
-Invader6=SpaceInvader(Invaderlocx+225,Invaderlocy,50,30,'images/spaceinvader.png')
-Invader7=SpaceInvader(Invaderlocx+270,Invaderlocy,50,30,'images/spaceinvader.png')
-bullets=Bullet(1000,1000,30,30,'images/bullet.png')
-#Button_1=button.no_background(5, 0, "Arial",20,(0,0,0),(255,0,0),"exit",exit_button)
-
+window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT), pygame.HWSURFACE)
+playersprite = Character(PLAYER_X, PLAYER_Y, 30, 30, 'images/imgoated.png')
+Area = area(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, 255)
+bg = Background(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, 255)
+bullets = Bullet(1000, 1000, 30, 30, 'images/bullet.png')
+invader_bullets = Invader_Bullet(1000, 1000, 30, 30, 'images/bullet.png')
 
 
 Playarea = pygame.sprite.Group()
 Player = pygame.sprite.GroupSingle()
 Enemy = pygame.sprite.Group()
-bullet=pygame.sprite.Group()
-Playarea.add(bg)
+bullet = pygame.sprite.Group()
+invaderbullets = pygame.sprite.Group()
+background = pygame.sprite.GroupSingle()
+Playarea.add(Area)
 Player.add(playersprite)
-Enemy.add(Invader,Invader2,Invader3,Invader4,Invader5,Invader6)
+background.add(bg)
+
+invaders_ROW_1 = [SpaceInvader(INVADER_X + i * 45, INVADER_Y, 30, 20, 'images/spaceinvader.png') for i in range(8)]
+invaders_ROW_2 = [SpaceInvader(INVADER_X + i * 45, INVADER_Y + 80, 30, 20, 'images/spaceinvader.png') for i in range(8)]
+invaders_ROW_3 = [SpaceInvader(INVADER_X + i * 45, INVADER_Y + 160, 30, 20, 'images/spaceinvader.png') for i in range(8)]
+Enemy.add(invaders_ROW_1)
+Enemy.add(invaders_ROW_2)
+Enemy.add(invaders_ROW_3)
+invaderbullets.add(invader_bullets)
 bullet.add(bullets)
 
 pygame.display.set_caption("Space Invaders")
+
+# Add a variable to track if the player is hit
+is_player_hit = False
+
 def display():
-    window.fill((255,255,255)) #White background
+    window.fill((255, 255, 255))  # White background
+    background.draw(window)
     Playarea.draw(window)
     Enemy.draw(window)
     Player.draw(window)
     bullet.draw(window)
-    Invader.move()
-    Invader2.move()
-    Invader3.move()
-    Invader4.move()
-    Invader5.move()
-    Invader6.move()
-    Invader7.move()
-    # Invader4.move()
-    
-    
+    invaderbullets.draw(window)
+
+    for invader in Enemy:
+        invader.move()
+
+# Add a timer to control the frequency of shooting
+shoot_timer = 0
+shoot_frequency = 40  # Adjust this value to control shooting frequency
+is_invader_shooting = False
+
 while True:
     display()
-    playersprite.move(3)    
-    
-    
-    
-    # if pygame.sprite.spritecollide(bg, Enemy, False, collided=pygame.sprite.collide_mask):
-    #     row+=1
+    playersprite.move(3)
     bullet.update()
-    for event in pygame.event.get():
+    invader_bullets.update()
+
+    for bullet_shot in bullet.sprites():
+        bullet_shot.move()
+        bullet_shot.check_collision(Enemy)
+        bullets.kill()
+
+    if len(Enemy) == 0:
+        font = pygame.font.SysFont('Consolas', 30)
+        window.blit(font.render("You've Won! GG ez", True, (255, 255, 255)), (100, 250))
+        playersprite.kill()
+        bullets.kill()
+
+    for invader in Enemy:
+        if invader.SI_check_hit_X(Playarea):
+            invader.movex = -invader.movex
+            invader.rect.y += 40
+
+    # Update the timer
+    shoot_timer = (shoot_timer + 1) % shoot_frequency
+
+    if shoot_timer == 0:
+        # Randomly choose if an invader should shoot
+        should_shoot = random.choice([True, False])
+
+        if should_shoot:
+            # Randomly choose one Space Invader to shoot
+            shooting_invader = random.choice(Enemy.sprites())
+            invader_bullet = Invader_Bullet(shooting_invader.rect.x, shooting_invader.rect.y, 30, 30,'images/bullet.png')
+            invaderbullets.add(invader_bullet)
+
+    for invader_bullet in invaderbullets.sprites():
+        invader_bullet.move()  # Move the bullets downwards
+
+    # Check collision with the player
+    if pygame.sprite.spritecollide(playersprite, invaderbullets, True):
+        is_player_hit = True
+        playersprite.kill()  # Kill the player when hit
         
+
+    if invader_bullet.rect.y > WINDOW_HEIGHT:
+        invader_bullet.kill()  # Remove bullets that go off-screen
+
+    for event in pygame.event.get():
         key_input = pygame.key.get_pressed()
         if key_input[pygame.K_SPACE]:
-            bullets=Bullet(playersprite.rect.x,playersprite.rect.y,30,30,255)
+            bullets = Bullet(playersprite.rect.x, playersprite.rect.y, 30, 30, 'images/bullet.png')
             bullet.add(bullets)
-            bullet.draw(window)
-            bullets.move()
-            
+
         # if user QUIT then the screen will close
-    if event.type == pygame.QUIT:
+        if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-        
-    
-    
-    if Invader.SI_check_hit_X(Playarea):
-            Invader.movex=-Invader.movex
-            Invader.rect.y+=40
-            Invader2.movex=-Invader2.movex
-            Invader2.rect.y+=40
-            Invader3.movex=-Invader3.movex
-            Invader3.rect.y+=40
-            Invader4.movex=-Invader4.movex
-            Invader4.rect.y+=40
-            Invader5.movex=-Invader5.movex
-            Invader5.rect.y+=40
-            
-    if Invader5.SI_check_hit_X(Playarea):
-            Invader.movex=-Invader.movex
-            Invader.rect.y+=40
-            Invader2.movex=-Invader2.movex
-            Invader2.rect.y+=40
-            Invader3.movex=-Invader3.movex
-            Invader3.rect.y+=40
-            Invader4.movex=-Invader4.movex
-            Invader4.rect.y+=40
-            Invader5.movex=-Invader5.movex
-            Invader5.rect.y+=40
+
     if playersprite.plr_check_hit(Playarea):
-            playersprite.plr_collide()
-        
-    pygame.display.update() #update the display
-    fpsClock.tick(fps) #speed of redraw
+        playersprite.plr_collide()
+
+    # Display the "You've Lost!" message when the player is hit
+    if is_player_hit:
+        font = pygame.font.SysFont('Consolas', 20)
+        window.blit(font.render("You've Lost!", True, (255, 255, 255)), (100, 250))
+        window.blit(font.render("LOL get better or beat it chump!!", True, (255, 255, 255)), (100, 280))
+    pygame.display.update()  # update the display
+    fpsClock.tick(fps)  # speed of redraw
+
         
     
     
