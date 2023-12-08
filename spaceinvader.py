1b1bcd16d68f985bdd2a04865f0c2c5db5fbc4c0
kinziq <144268675+kinziq@users.noreply.github.com>
2023-12-08 11:40:00 -0500

    Update spaceinvader.py

--- spaceinvader.py
+++ spaceinvader.py
@@ -1,6 +1,6 @@
 import pygame
 import sys
-#custom object for copys of walls        
+#custom object for copys of walls
 class SpaceInvader(pygame.sprite.Sprite):
     def __init__(self, startX,startY,width,height,path_images):
         super().__init__()
@@ -9,17 +9,17 @@ class SpaceInvader(pygame.sprite.Sprite):
         self.mask = pygame.mask.from_surface(self.image)
         self.rect = self.image.get_rect(topleft=(startX,startY))
         self.movex = 2
-
+        self.shoot_cooldown = 2000  # Set the cooldown period in milliseconds
+        self.last_shot_time = 0
+    
     def move(self):
         self.rect.x+=self.movex
-        
+    
     def SI_check_hit_X(self,group):
         if pygame.sprite.spritecollide(self, group, False, collided=pygame.sprite.collide_mask):
             return True
         else:
-            return
-        
-        
+            return    
         
     def SI_collide_X(self):
         self.rect.x-=self.movex
